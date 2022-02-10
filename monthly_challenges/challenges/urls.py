d1fbf7027748bed51d492579d215d563772ed4ad
Teodor Wisniewski <teodor.wisniewski@altmansolon.com>
2022-02-10 13:01:06 +0100

    dynamic urls

---
+++ monthly_challenges/challenges/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, re_path
+from . import views
+
+urlpatterns = [
+    path("", views.index),
+    path("<int:month>", views.montly_challenge_by_number),
+    path("<str:month>", views.montly_challenge),
+    re_path(r'^test/(?P<var>\D+)$', views.test),
+]
+
+
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
-# Create your views here.
+montly_challenges = {
+    1: "Don't eat sugar!",
+    2: "Don't eat carbs!",
+    3: "Stay in keto",
+    4: "Read about flexible keto",
+    5: "increase carbs",
+    6: "leave keto",
+}
+
+def index(request):
+    return HttpResponse("Choose your month challenge!")
+
+
+def test(request, var):
+    return HttpResponse(f"What we got {var}")
+
+
+
+def montly_challenge_by_number(request, month):
+
+    challenge_text = montly_challenges.get(month, HttpResponseNotFound("This month is not supported"))
+    return HttpResponse(challenge_text)
+
+
+def montly_challenge(request, month):
+    challenge_text = montly_challenges.get(month, HttpResponseNotFound("This month is not supported"))
+    return HttpResponse(challenge_text)
