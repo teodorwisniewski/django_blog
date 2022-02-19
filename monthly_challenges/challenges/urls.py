16984a4b2e5e93213bed64d2d38725627a81252a
Teodor <teodorwis90@gmail.com>
2022-02-19 19:57:47 +0100

    base tamplate partial templates

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="index"),
     path("<int:month>", views.montly_challenge_by_number),
     path("<str:month>", views.montly_challenge, name="month-challenge"),
     re_path(r'^test/(?P<var>\D+)$', views.test),
